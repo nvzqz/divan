e278595b656d85c871592e15facef489068257ae
Nikolai Vazquez <hello@nikolaivazquez.com>
2023-06-30 02:18:15 -0400

    Create math benchmark examples

---
+++ examples/benches/math.rs
@@ -0,0 +1,30 @@
+#![allow(dead_code)]
+
+use std::hint::black_box as bb;
+
+fn main() {}
+
+#[divan::bench]
+fn add() {
+    _ = bb(bb(2) + bb(1));
+}
+
+#[divan::bench]
+fn sub() {
+    _ = bb(bb(2) - bb(1));
+}
+
+#[divan::bench]
+fn mul() {
+    _ = bb(bb(2) * bb(1));
+}
+
+#[divan::bench]
+fn div() {
+    _ = bb(bb(2) / bb(1));
+}
+
+#[divan::bench]
+fn rem() {
+    _ = bb(bb(2) % bb(1));
+}
