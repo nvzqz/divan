40807f1da43e6ec8421e91d65718d8189fbb98d7
Nikolai Vazquez <hello@nikolaivazquez.com>
2023-06-30 02:06:58 -0400

    Add `#[divan::bench]` scaffold

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,3 +2,22 @@
 //! statistically-comfy benchmarking library brought to you by [Nikolai Vazquez](https://hachyderm.io/@nikolai).
 
 #![warn(missing_docs)]
+
+use proc_macro::TokenStream;
+
+/// Registers a benchmarking function.
+///
+/// # Examples
+///
+/// ```
+/// use std::hint::black_box as bb;
+///
+/// #[divan::bench]
+/// fn add() {
+///     bb(bb(1) + bb(42));
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn bench(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
