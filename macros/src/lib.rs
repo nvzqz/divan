72017d64ffb73468255e47416418e386478d96df
Nikolai Vazquez <hello@nikolaivazquez.com>
2023-07-02 06:10:40 -0400

    Add `#[bench(crate = path::to::divan)]`
    This allows macros from other crates to use `divan` internally.

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -22,7 +22,40 @@ use proc_macro::TokenStream;
 ///     divan::main();
 /// }
 /// ```
+///
+/// # Options
+///
+/// - `#[divan::bench(crate = path::to::divan)]`
+///
+///   The path to the specific `divan` crate instance used by this macro's
+///   generated code can be specified via the `crate` option. This is applicable
+///   when using `divan` via a macro from your own crate.
+///
+///   ```
+///   extern crate divan as sofa;
+///
+///   #[::sofa::bench(crate = ::sofa)]
+///   fn add() {
+///       // ...
+///   }
+///   ```
 #[proc_macro_attribute]
-pub fn bench(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn bench(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut divan_crate = None::<syn::Path>;
+
+    let attr_parser = syn::meta::parser(|meta| {
+        if meta.path.is_ident("crate") {
+            divan_crate = Some(meta.value()?.parse()?);
+            Ok(())
+        } else {
+            Err(meta.error("unsupported 'bench' property"))
+        }
+    });
+
+    syn::parse_macro_input!(attr with attr_parser);
+
+    // All access to `divan` must go through this path.
+    let _divan_crate = divan_crate.unwrap_or_else(|| syn::parse_quote!(::divan));
+
     item
 }
