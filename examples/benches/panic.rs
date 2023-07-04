c47df5e014f9ce65602485d1bbaf9a9981e98892
Nikolai Vazquez <hello@nikolaivazquez.com>
2023-07-03 22:09:30 -0400

    Add panic-related example benchmarks

---
+++ examples/benches/panic.rs
@@ -0,0 +1,30 @@
+use std::{hint::black_box as bb, panic};
+
+fn main() {
+    // Silence panics.
+    panic::set_hook(Box::new(|_| {}));
+
+    divan::main();
+}
+
+#[divan::bench]
+#[track_caller]
+fn caller_location() -> &'static panic::Location<'static> {
+    panic::Location::caller()
+}
+
+mod catch_unwind {
+    use super::*;
+
+    #[divan::bench]
+    fn panic() -> std::thread::Result<()> {
+        let panic: fn() = || panic!();
+        panic::catch_unwind(bb(panic))
+    }
+
+    #[divan::bench]
+    fn success() -> std::thread::Result<()> {
+        let success: fn() = || {};
+        panic::catch_unwind(bb(success))
+    }
+}
