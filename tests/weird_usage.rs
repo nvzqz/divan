dd38013e4e3bcae872d9cef841015a0ed47571fd
Nikolai Vazquez <hello@nikolaivazquez.com>
2023-07-04 23:27:35 -0400

    Test multiple `#[divan::bench]` on same function

--- tests/weird_usage.rs
+++ tests/weird_usage.rs
@@ -21,18 +21,37 @@ fn embedded() {
 #[divan::bench]
 fn r#raw_ident() {}
 
+#[divan::bench]
+#[divan::bench]
+fn twice() {}
+
+#[divan::bench]
+#[divan::bench]
+#[divan::bench]
+fn thrice() {}
+
 // Test that each function appears the expected number of times.
 #[test]
 fn count() {
     let mut inner_count = 0;
+    let mut twice_count = 0;
+    let mut thrice_count = 0;
 
     for entry in ENTRIES {
         if entry.path.contains("inner") {
             inner_count += 1;
         }
+        if entry.path.contains("twice") {
+            twice_count += 1;
+        }
+        if entry.path.contains("thrice") {
+            thrice_count += 1;
+        }
     }
 
     assert_eq!(inner_count, 2);
+    assert_eq!(twice_count, 2);
+    assert_eq!(thrice_count, 3);
 }
 
 // Test expected `Entry.path` values.
@@ -58,6 +77,10 @@ fn unique_id() {
     let mut seen = HashSet::new();
 
     for entry in ENTRIES {
+        if entry.path.contains("twice") || entry.path.contains("thrice") {
+            continue;
+        }
+
         let id = (entry.get_id)();
         assert!(seen.insert(id), "Function '{}' does not have a unique type ID", entry.path);
     }
