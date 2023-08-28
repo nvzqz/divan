bb92538589c6f73fdd52bd06326866e7762b5a24
Nikolai Vazquez <hello@nikolaivazquez.com>
2023-08-28 03:28:31 -0400

    Make `collections::util` module its own file

---
+++ examples/benches/util.rs
@@ -0,0 +1,38 @@
+use std::collections::{BTreeSet, BinaryHeap, HashSet, LinkedList, VecDeque};
+
+use divan::black_box;
+
+pub fn collect_nums<T: FromIterator<i32>>(n: usize) -> T {
+    black_box(0..(n as i32)).collect()
+}
+
+pub trait WithCapacity {
+    fn with_capacity(c: usize) -> Self;
+}
+
+pub trait Clear {
+    fn clear(&mut self);
+}
+
+macro_rules! impl_with_capacity {
+    ($($t:ident),+) => {
+        $(impl WithCapacity for $t<i32> {
+            fn with_capacity(c: usize) -> Self {
+                $t::with_capacity(c)
+            }
+        })+
+    };
+}
+
+macro_rules! impl_clear {
+    ($($t:ident),+) => {
+        $(impl Clear for $t<i32> {
+            fn clear(&mut self) {
+                $t::clear(self);
+            }
+        })+
+    };
+}
+
+impl_with_capacity!(Vec, VecDeque, BinaryHeap, HashSet);
+impl_clear!(Vec, VecDeque, BinaryHeap, HashSet, LinkedList, BTreeSet);
