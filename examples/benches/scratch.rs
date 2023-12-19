e0d6032868c1b9a16717725601595076c78ec5ea
Nikolai Vazquez <hello@nikolaivazquez.com>
2023-12-19 01:27:48 -0500

    Add commented-out `AllocProfiler` to `scratch.rs`
    This makes it easier to get started with `AllocProfiler` when coming
    from the blog post or when doing ad-hoc benchmarking.

--- examples/benches/scratch.rs
+++ examples/benches/scratch.rs
@@ -6,6 +6,10 @@
 //! cargo bench -q -p examples --bench scratch
 //! ```
 
+// Uncomment the code below to measure heap allocations.
+// #[global_allocator]
+// static ALLOC: divan::AllocProfiler = divan::AllocProfiler::system();
+
 fn main() {
     divan::main();
 }
