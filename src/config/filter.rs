nvzqz/divan

commit 07111fb6f82b307a38d0d26da842436504bce0fb

Abstract filters into `FilterSet`
Improved size and cache locality of filters by storing all filters
together and reducing the filters' inline footprint by 2 words.

Improved code generation for matching by sharing code for positive and
negative filters.

Replaced `SkipRegex` with a simpler `IntoRegex` trait.

Added `SplitVec`, which partitions elements on a condition.

Added tests for filtering.

src/config/filter.rs
@@ -1,0 +1,166 @@
+use regex::Regex;
+
+use crate::util::split_vec::SplitVec;
+
+/// Filters which benchmark/group to run based on its path.
+pub(crate) enum Filter {
+    Regex(Regex),
+    Exact(String),
+}
+
+impl Filter {
+    fn is_match(&self, s: &str) -> bool {
+        match self {
+            Self::Regex(regex) => regex.is_match(s),
+            Self::Exact(exact) => exact == s,
+        }
+    }
+}
+
+/// Collection of positive and negative filters.
+///
+/// Positive filters indicate that a benchmark/group path should be included,
+/// whereas negative filters exclude entries. Negative filters are placed before
+/// positive filters because they have priority.
+#[derive(Default)]
+pub(crate) struct FilterSet {
+    filters: SplitVec<Filter>,
+}
+
+impl FilterSet {
+    #[inline]
+    pub fn reserve_exact(&mut self, additional: usize) {
+        self.filters.reserve_exact(additional);
+    }
+
+    pub fn insert(&mut self, filter: Filter, positive: bool) {
+        self.filters.insert(filter, positive);
+    }
+
+    /// Returns `true` if a benchmark/group path matches these filters, and thus
+    /// the entry should be included.
+    ///
+    /// Negative filters are prioritized over positive filters.
+    pub fn is_match(&self, entry_path: &str) -> bool {
+        let filters = self.filters.all();
+        let positive_start = self.filters.split_index();
+
+        // If any filter matches, return whether it was positive or negative.
+        // Negative filters are placed before positive filters because they have
+        // priority.
+        if let Some(index) = filters.iter().position(|f| f.is_match(entry_path)) {
+            return index >= positive_start;
+        }
+
+        // Otherwise succeed only if there are no positive filters.
+        filters.len() == positive_start
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Empty filter sets should match all strings.
+    #[test]
+    fn empty() {
+        let filters = FilterSet::default();
+        assert!(filters.is_match("abc"));
+        assert!(filters.is_match("123"));
+    }
+
+    mod single {
+        use super::*;
+
+        #[test]
+        fn positive_exact() {
+            let mut filters = FilterSet::default();
+
+            filters.insert(Filter::Exact("abc".into()), true);
+
+            assert!(filters.is_match("abc"));
+            assert!(!filters.is_match("ab"));
+            assert!(!filters.is_match("abcd"));
+        }
+
+        #[test]
+        fn negative_exact() {
+            let mut filters = FilterSet::default();
+
+            filters.insert(Filter::Exact("abc".into()), false);
+
+            assert!(!filters.is_match("abc"));
+            assert!(filters.is_match("ab"));
+            assert!(filters.is_match("abcd"));
+        }
+
+        #[test]
+        fn positive_regex() {
+            let mut filters = FilterSet::default();
+            let regex = Regex::new("abc.*123").unwrap();
+
+            filters.insert(Filter::Regex(regex), true);
+
+            assert!(!filters.is_match("abc"));
+            assert!(filters.is_match("abc123"));
+            assert!(filters.is_match("abc::123"));
+        }
+
+        #[test]
+        fn negative_regex() {
+            let mut filters = FilterSet::default();
+            let regex = Regex::new("abc.*123").unwrap();
+
+            filters.insert(Filter::Regex(regex), false);
+
+            assert!(filters.is_match("abc"));
+            assert!(!filters.is_match("abc123"));
+            assert!(!filters.is_match("abc::123"));
+        }
+    }
+
+    /// Multiple positive filters should not be restrictive, whereas negative
+    /// filters are increasingly restrictive.
+    mod multi {
+        use super::*;
+
+        #[test]
+        fn exact() {
+            let mut filters = FilterSet::default();
+
+            filters.insert(Filter::Exact("abc".into()), true);
+            filters.insert(Filter::Exact("123".into()), true);
+
+            assert!(filters.is_match("abc"));
+            assert!(filters.is_match("123"));
+            assert!(!filters.is_match("xyz"));
+        }
+    }
+
+    /// Negative filters override positive filters.
+    mod overridden {
+        use super::*;
+
+        #[test]
+        fn exact() {
+            let mut filters = FilterSet::default();
+
+            filters.insert(Filter::Exact("abc".into()), true);
+            filters.insert(Filter::Exact("abc".into()), false);
+
+            assert!(!filters.is_match("abc"));
+        }
+
+        #[test]
+        fn regex() {
+            let mut filters = FilterSet::default();
+            let regex = Regex::new("abc.*123").unwrap();
+
+            filters.insert(Filter::Regex(regex.clone()), true);
+            filters.insert(Filter::Regex(regex), false);
+
+            assert!(!filters.is_match("abc::123"));
+            assert!(!filters.is_match("123::abc"));
+        }
+    }
+}

src/divan.rs
@@ -7,7 +7,10 @@ use regex::Regex;
 
 use crate::{
     benchmark::BenchOptions,
-    config::{Action, Filter, ParsedSeconds, RunIgnored, SortingAttr},
+    config::{
+        filter::{Filter, FilterSet},
+        Action, ParsedSeconds, RunIgnored, SortingAttr,
+    },
     counter::{
         BytesCount, BytesFormat, CharsCount, CyclesCount, IntoCounter, ItemsCount, MaxCountUInt,
         PrivBytesFormat,
@@ -16,7 +19,7 @@ use crate::{
     thread_pool::BENCH_POOL,
     time::{Timer, TimerKind},
     tree_painter::{TreeColumn, TreePainter},
-    util::{self, defer},
+    util::{self, defer, IntoRegex},
     Bencher,
 };
 
@@ -29,8 +32,7 @@ pub struct Divan {
     sorting_attr: SortingAttr,
     color: ColorChoice,
     bytes_format: BytesFormat,
-    filters: Vec<Filter>,
-    skip_filters: Vec<Filter>,
+    filters: FilterSet,
     run_ignored: RunIgnored,
     bench_options: BenchOptions<'static>,
 }
@@ -82,13 +84,7 @@ impl Divan {
     /// This does not take into account `entry.ignored` because that is handled
     /// separately.
     fn filter(&self, entry_path: &str) -> bool {
-        if !self.filters.is_empty()
-            && !self.filters.iter().any(|filter| filter.is_match(entry_path))
-        {
-            return false;
-        }
-
-        !self.skip_filters.iter().any(|filter| filter.is_match(entry_path))
+        self.filters.is_match(entry_path)
     }
 
     pub(crate) fn should_ignore(&self, ignored: bool) -> bool {
@@ -392,31 +388,6 @@ impl Divan {
     }
 }
 
-/// Makes `Divan::skip_regex` input polymorphic.
-pub trait SkipRegex {
-    fn skip_regex(self, divan: &mut Divan);
-}
-
-impl SkipRegex for Regex {
-    fn skip_regex(self, divan: &mut Divan) {
-        divan.skip_filters.push(Filter::Regex(self));
-    }
-}
-
-impl SkipRegex for &str {
-    #[track_caller]
-    fn skip_regex(self, divan: &mut Divan) {
-        Regex::new(self).unwrap().skip_regex(divan);
-    }
-}
-
-impl SkipRegex for String {
-    #[track_caller]
-    fn skip_regex(self, divan: &mut Divan) {
-        self.as_str().skip_regex(divan)
-    }
-}
-
 /// Configuration options.
 impl Divan {
     /// Creates an instance with options set by parsing CLI arguments.
@@ -431,29 +402,43 @@ impl Divan {
     pub fn config_with_args(mut self) -> Self {
         let mut command = crate::cli::command();
 
-        let matches = command.get_matches_mut();
+        let mut matches = command.get_matches_mut();
         let is_exact = matches.get_flag("exact");
 
-        let mut parse_filter = |filter: &String| {
-            if is_exact {
-                Filter::Exact(filter.to_owned())
-            } else {
-                match Regex::new(filter) {
-                    Ok(r) => Filter::Regex(r),
-                    Err(error) => {
+        // Insert filters.
+        {
+            let mut parse_filter = |filter: String| -> Filter {
+                if is_exact {
+                    Filter::Exact(filter)
+                } else {
+                    Filter::Regex(Regex::new(&filter).unwrap_or_else(|error| {
                         let kind = clap::error::ErrorKind::ValueValidation;
                         command.error(kind, error).exit();
-                    }
+                    }))
+                }
+            };
+
+            let positive_filters = matches.remove_many::<String>("filter");
+            let negative_filters = matches.remove_many::<String>("skip");
+
+            // Reduce allocation size and reallocation count.
+            self.filters.reserve_exact({
+                let positive_count = positive_filters.as_ref().map(|f| f.len()).unwrap_or_default();
+                let negative_count = negative_filters.as_ref().map(|f| f.len()).unwrap_or_default();
+                positive_count + negative_count
+            });
+
+            if let Some(positive_filters) = positive_filters {
+                for filter in positive_filters {
+                    self.filters.insert(parse_filter(filter), true);
                 }
             }
-        };
 
-        if let Some(filters) = matches.get_many::<String>("filter") {
-            self.filters.extend(filters.map(&mut parse_filter));
-        }
-
-        if let Some(skip_filters) = matches.get_many::<String>("skip") {
-            self.skip_filters.extend(skip_filters.map(&mut parse_filter));
+            if let Some(negative_filters) = negative_filters {
+                for filter in negative_filters {
+                    self.filters.insert(parse_filter(filter), false);
+                }
+            }
         }
 
         self.action = if matches.get_flag("list") {
@@ -621,8 +606,9 @@ impl Divan {
     ///
     /// Panics if `filter` is a string and [`Regex::new`] fails.
     #[must_use]
-    pub fn skip_regex(mut self, filter: impl SkipRegex) -> Self {
-        filter.skip_regex(&mut self);
+    #[track_caller]
+    pub fn skip_regex(mut self, filter: impl IntoRegex) -> Self {
+        self.filters.insert(Filter::Regex(filter.into_regex()), true);
         self
     }
 
@@ -650,7 +636,7 @@ impl Divan {
     /// ```
     #[must_use]
     pub fn skip_exact(mut self, filter: impl Into<String>) -> Self {
-        self.skip_filters.push(Filter::Exact(filter.into()));
+        self.filters.insert(Filter::Exact(filter.into()), true);
         self
     }
 

src/config/mod.rs
@@ -1,8 +1,8 @@
 use std::{cmp::Ordering, error::Error, str::FromStr, time::Duration};
 
-use regex::Regex;
-
 use crate::util::sort::natural_cmp;
+
+pub mod filter;
 
 /// `Duration` wrapper for parsing seconds from the CLI.
 #[derive(Clone, Copy)]
@@ -55,22 +55,6 @@ impl Action {
     #[inline]
     pub fn is_list_terse(&self) -> bool {
         matches!(self, Self::ListTerse)
-    }
-}
-
-/// Filters which benchmark to run based on name.
-pub(crate) enum Filter {
-    Regex(Regex),
-    Exact(String),
-}
-
-impl Filter {
-    /// Returns `true` if a string matches this filter.
-    pub fn is_match(&self, s: &str) -> bool {
-        match self {
-            Self::Regex(r) => r.is_match(s),
-            Self::Exact(e) => e == s,
-        }
     }
 }
 

src/lib.rs
@@ -18,6 +18,9 @@
 #[path = "private.rs"]
 pub mod __private;
 
+#[macro_use]
+mod util;
+
 mod alloc;
 mod benchmark; // NOTE: "bench" would be imported into the prelude.
 mod cli;
@@ -29,7 +32,6 @@ mod stats;
 mod thread_pool;
 mod time;
 mod tree_painter;
-mod util;
 
 pub mod counter;
 

src/util/macros.rs
@@ -1,0 +1,11 @@
+/// `assert!` that's only checked in debug builds and is otherwise an
+/// optimization hint in release builds.
+macro_rules! assert_unchecked {
+    ($condition:expr $(, $message:expr)* $(,)?) => {
+        if cfg!(any(debug_assertions, miri)) {
+            assert!($condition $(, $message)*);
+        } else {
+            $crate::util::assert_unchecked($condition);
+        }
+    }
+}

src/util/mod.rs
@@ -4,8 +4,14 @@ use std::{
     sync::atomic::{AtomicUsize, Ordering::Relaxed},
 };
 
+use regex::Regex;
+
+#[macro_use]
+mod macros;
+
 pub mod fmt;
 pub mod sort;
+pub mod split_vec;
 pub mod sync;
 pub mod thread;
 pub mod ty;
@@ -16,6 +22,36 @@ pub mod ty;
 /// working with `()` unintentionally.
 #[non_exhaustive]
 pub struct Unit;
+
+/// Public-in-private trait to make `DivanConfig::skip_regex` polymorphic over
+/// regular expression types.
+pub trait IntoRegex {
+    fn into_regex(self) -> Regex;
+}
+
+impl IntoRegex for &str {
+    #[inline]
+    #[track_caller]
+    fn into_regex(self) -> Regex {
+        Regex::new(self).unwrap()
+    }
+}
+
+impl IntoRegex for String {
+    #[inline]
+    #[track_caller]
+    fn into_regex(self) -> Regex {
+        Regex::new(&self).unwrap()
+    }
+}
+
+/// [`std::hint::assert_unchecked`] polyfill.
+#[inline]
+pub(crate) const unsafe fn assert_unchecked(cond: bool) {
+    if !cond {
+        std::hint::unreachable_unchecked();
+    }
+}
 
 #[inline]
 pub(crate) fn defer<F: FnOnce()>(f: F) -> impl Drop {

src/util/split_vec.rs
@@ -1,0 +1,135 @@
+/// `Vec` partitioned in half.
+///
+/// This type exists to make `FilterSet` have a smaller footprint and for
+/// `FilterSet::is_match` to generate better code.
+pub(crate) struct SplitVec<T> {
+    items: Vec<T>,
+    split_index: usize,
+}
+
+impl<T> Default for SplitVec<T> {
+    #[inline]
+    fn default() -> Self {
+        Self { items: Vec::default(), split_index: 0 }
+    }
+}
+
+impl<T> SplitVec<T> {
+    /// Inserts an item to the end of either the first or second half.
+    #[inline]
+    pub fn insert(&mut self, value: T, after_split: bool) {
+        unsafe {
+            // Ensure we have at least one slot available.
+            self.items.reserve(1);
+
+            let old_len = self.items.len();
+            let old_split = self.split_index();
+
+            let start_ptr = self.items.as_mut_ptr();
+            let last_ptr = start_ptr.add(old_len);
+            let split_ptr = start_ptr.add(old_split);
+            let value_slot = if after_split { last_ptr } else { split_ptr };
+
+            // If writing to before the split, then increment the split index
+            // and move any value there to the end.
+            //
+            // NOTE: We can't use `copy_to_nonoverlapping` because both pointers
+            // are the same if `old_len` is 0.
+            if !after_split {
+                split_ptr.copy_to(last_ptr, 1);
+                self.set_split_index(old_split + 1);
+            }
+
+            value_slot.write(value);
+            self.items.set_len(old_len + 1);
+        }
+    }
+
+    #[inline]
+    pub fn reserve_exact(&mut self, additional: usize) {
+        self.items.reserve_exact(additional);
+    }
+
+    /// Returns the slice of all items.
+    #[inline]
+    pub fn all(&self) -> &[T] {
+        &self.items
+    }
+
+    /// Returns the split halves.
+    #[inline]
+    #[cfg(test)]
+    pub fn split(&self) -> (&[T], &[T]) {
+        self.items.split_at(self.split_index())
+    }
+
+    /// Returns where the halves are split.
+    #[inline]
+    pub fn split_index(&self) -> usize {
+        let index = self.split_index;
+
+        // Optimization hint to remove bounds checks.
+        let len = self.items.len();
+        unsafe { assert_unchecked!(index <= len, "index {index} out of bounds (len = {len})") }
+
+        index
+    }
+
+    /// Sets where the halves are split.
+    #[inline]
+    pub unsafe fn set_split_index(&mut self, new_index: usize) {
+        self.split_index = new_index;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[track_caller]
+    fn test(vec: &SplitVec<&str>, before: &[&str], after: &[&str]) {
+        assert_eq!(vec.split(), (before, after));
+    }
+
+    #[test]
+    fn before_split() {
+        let mut vec = SplitVec::<&str>::default();
+
+        vec.insert("abc", false);
+        test(&vec, &["abc"], &[]);
+
+        vec.insert("xyz", false);
+        test(&vec, &["abc", "xyz"], &[]);
+    }
+
+    #[test]
+    fn after_split() {
+        let mut vec = SplitVec::<&str>::default();
+
+        vec.insert("abc", true);
+        test(&vec, &[], &["abc"]);
+
+        vec.insert("xyz", true);
+        test(&vec, &[], &["abc", "xyz"]);
+    }
+
+    #[test]
+    fn mixed() {
+        let mut vec = SplitVec::<&str>::default();
+
+        vec.insert("abc", false);
+        test(&vec, &["abc"], &[]);
+
+        vec.insert("xyz", true);
+        test(&vec, &["abc"], &["xyz"]);
+
+        vec.insert("123", false);
+        test(&vec, &["abc", "123"], &["xyz"]);
+
+        vec.insert("456", true);
+        test(&vec, &["abc", "123"], &["xyz", "456"]);
+
+        vec.insert("789", false);
+        test(&vec, &["abc", "123", "789"], &["456", "xyz"]);
+    }
+}

commit 5afb0954869a590be8b15cfa1f0a01582c3a7d84

Fix broken `Divan::skip_exact`
Also improve handling of filters.

This breakage was introduced in commit 07111fb.

src/config/filter.rs
@@ -17,13 +17,17 @@ impl Filter {
     }
 }
 
-/// Collection of positive and negative filters.
+/// Collection of inclusive and exclusive filters.
 ///
-/// Positive filters indicate that a benchmark/group path should be included,
-/// whereas negative filters exclude entries. Negative filters are placed before
-/// positive filters because they have priority.
+/// Inclusive filters indicate that a benchmark/group path should be run without
+/// running other benchmarks (unless also included).
+///
+/// Exclusive filters make all matching candidate benchmarks be skipped (even if
+/// explicitly included). As a result, they have priority over inclusive
+/// filters.
 #[derive(Default)]
 pub(crate) struct FilterSet {
+    /// Stores exclusive filters followed by inclusive filters.
     filters: SplitVec<Filter>,
 }
 
@@ -33,27 +37,37 @@ impl FilterSet {
         self.filters.reserve_exact(additional);
     }
 
-    pub fn insert(&mut self, filter: Filter, positive: bool) {
-        self.filters.insert(filter, positive);
+    #[inline]
+    pub fn include(&mut self, filter: Filter) {
+        self.insert_filter(filter, true);
+    }
+
+    #[inline]
+    pub fn exclude(&mut self, filter: Filter) {
+        self.insert_filter(filter, false);
+    }
+
+    fn insert_filter(&mut self, filter: Filter, inclusive: bool) {
+        self.filters.insert(filter, inclusive);
     }
 
     /// Returns `true` if a benchmark/group path matches these filters, and thus
     /// the entry should be included.
     ///
-    /// Negative filters are prioritized over positive filters.
+    /// Negative filters are prioritized over inclusive filters.
     pub fn is_match(&self, entry_path: &str) -> bool {
         let filters = self.filters.all();
-        let positive_start = self.filters.split_index();
+        let inclusive_start = self.filters.split_index();
 
-        // If any filter matches, return whether it was positive or negative.
-        // Negative filters are placed before positive filters because they have
+        // If any filter matches, return whether it was inclusive or negative.
+        // Negative filters are placed before inclusive filters because they have
         // priority.
         if let Some(index) = filters.iter().position(|f| f.is_match(entry_path)) {
-            return index >= positive_start;
+            return index >= inclusive_start;
         }
 
-        // Otherwise succeed only if there are no positive filters.
-        filters.len() == positive_start
+        // Otherwise succeed only if there are no inclusive filters.
+        filters.len() == inclusive_start
     }
 }
 
@@ -73,10 +87,10 @@ mod tests {
         use super::*;
 
         #[test]
-        fn positive_exact() {
+        fn inclusive_exact() {
             let mut filters = FilterSet::default();
 
-            filters.insert(Filter::Exact("abc".into()), true);
+            filters.insert_filter(Filter::Exact("abc".into()), true);
 
             assert!(filters.is_match("abc"));
             assert!(!filters.is_match("ab"));
@@ -84,10 +98,10 @@ mod tests {
         }
 
         #[test]
-        fn negative_exact() {
+        fn exclusive_exact() {
             let mut filters = FilterSet::default();
 
-            filters.insert(Filter::Exact("abc".into()), false);
+            filters.insert_filter(Filter::Exact("abc".into()), false);
 
             assert!(!filters.is_match("abc"));
             assert!(filters.is_match("ab"));
@@ -95,11 +109,11 @@ mod tests {
         }
 
         #[test]
-        fn positive_regex() {
+        fn inclusive_regex() {
             let mut filters = FilterSet::default();
             let regex = Regex::new("abc.*123").unwrap();
 
-            filters.insert(Filter::Regex(regex), true);
+            filters.insert_filter(Filter::Regex(regex), true);
 
             assert!(!filters.is_match("abc"));
             assert!(filters.is_match("abc123"));
@@ -107,11 +121,11 @@ mod tests {
         }
 
         #[test]
-        fn negative_regex() {
+        fn exclusive_regex() {
             let mut filters = FilterSet::default();
             let regex = Regex::new("abc.*123").unwrap();
 
-            filters.insert(Filter::Regex(regex), false);
+            filters.insert_filter(Filter::Regex(regex), false);
 
             assert!(filters.is_match("abc"));
             assert!(!filters.is_match("abc123"));
@@ -119,7 +133,7 @@ mod tests {
         }
     }
 
-    /// Multiple positive filters should not be restrictive, whereas negative
+    /// Multiple inclusive filters should not be restrictive, whereas negative
     /// filters are increasingly restrictive.
     mod multi {
         use super::*;
@@ -128,8 +142,8 @@ mod tests {
         fn exact() {
             let mut filters = FilterSet::default();
 
-            filters.insert(Filter::Exact("abc".into()), true);
-            filters.insert(Filter::Exact("123".into()), true);
+            filters.insert_filter(Filter::Exact("abc".into()), true);
+            filters.insert_filter(Filter::Exact("123".into()), true);
 
             assert!(filters.is_match("abc"));
             assert!(filters.is_match("123"));
@@ -137,7 +151,7 @@ mod tests {
         }
     }
 
-    /// Negative filters override positive filters.
+    /// Negative filters override inclusive filters.
     mod overridden {
         use super::*;
 
@@ -145,8 +159,8 @@ mod tests {
         fn exact() {
             let mut filters = FilterSet::default();
 
-            filters.insert(Filter::Exact("abc".into()), true);
-            filters.insert(Filter::Exact("abc".into()), false);
+            filters.insert_filter(Filter::Exact("abc".into()), true);
+            filters.insert_filter(Filter::Exact("abc".into()), false);
 
             assert!(!filters.is_match("abc"));
         }
@@ -156,8 +170,8 @@ mod tests {
             let mut filters = FilterSet::default();
             let regex = Regex::new("abc.*123").unwrap();
 
-            filters.insert(Filter::Regex(regex.clone()), true);
-            filters.insert(Filter::Regex(regex), false);
+            filters.insert_filter(Filter::Regex(regex.clone()), true);
+            filters.insert_filter(Filter::Regex(regex), false);
 
             assert!(!filters.is_match("abc::123"));
             assert!(!filters.is_match("123::abc"));

src/divan.rs
@@ -418,25 +418,29 @@ impl Divan {
                 }
             };
 
-            let positive_filters = matches.remove_many::<String>("filter");
-            let negative_filters = matches.remove_many::<String>("skip");
+            let inclusive_filters = matches.remove_many::<String>("filter");
+            let exclusive_filters = matches.remove_many::<String>("skip");
 
             // Reduce allocation size and reallocation count.
             self.filters.reserve_exact({
-                let positive_count = positive_filters.as_ref().map(|f| f.len()).unwrap_or_default();
-                let negative_count = negative_filters.as_ref().map(|f| f.len()).unwrap_or_default();
-                positive_count + negative_count
+                let inclusive_count =
+                    inclusive_filters.as_ref().map(|f| f.len()).unwrap_or_default();
+
+                let exclusive_count =
+                    exclusive_filters.as_ref().map(|f| f.len()).unwrap_or_default();
+
+                inclusive_count + exclusive_count
             });
 
-            if let Some(positive_filters) = positive_filters {
-                for filter in positive_filters {
-                    self.filters.insert(parse_filter(filter), true);
+            if let Some(inclusive_filters) = inclusive_filters {
+                for filter in inclusive_filters {
+                    self.filters.include(parse_filter(filter));
                 }
             }
 
-            if let Some(negative_filters) = negative_filters {
-                for filter in negative_filters {
-                    self.filters.insert(parse_filter(filter), false);
+            if let Some(exclusive_filters) = exclusive_filters {
+                for filter in exclusive_filters {
+                    self.filters.exclude(parse_filter(filter));
                 }
             }
         }
@@ -608,7 +612,7 @@ impl Divan {
     #[must_use]
     #[track_caller]
     pub fn skip_regex(mut self, filter: impl IntoRegex) -> Self {
-        self.filters.insert(Filter::Regex(filter.into_regex()), false);
+        self.filters.exclude(Filter::Regex(filter.into_regex()));
         self
     }
 
@@ -636,7 +640,7 @@ impl Divan {
     /// ```
     #[must_use]
     pub fn skip_exact(mut self, filter: impl Into<String>) -> Self {
-        self.filters.insert(Filter::Exact(filter.into()), true);
+        self.filters.exclude(Filter::Exact(filter.into()));
         self
     }
 